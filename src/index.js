d6ddb2d3ee2a53c70cc631000bbc5e24c042c764
Dmitry Andrianov <dima.andrianov@gmail.com>
2019-06-13 16:22:55 -0700

    Create Dropdown.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import Tooltip from './ui/tooltip';
+import Dropdown from './ui/dropdown';
 
 
 //create a tooltip
 
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
 tooltip.init();
+
+// create a dropdown
+
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach( instance => {
+    const dropdown = new Dropdown(instance);
+    dropdown.init();
+})
